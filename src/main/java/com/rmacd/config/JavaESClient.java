dd1a55f00eb730b72cccc349f69b6b4626b31754
Ronald MacDonald <ronald@rmacd.com>
2024-05-05 00:19:46 +0100

    fix projection

--- src/main/java/com/rmacd/config/JavaESClient.java
+++ src/main/java/com/rmacd/config/JavaESClient.java
@@ -5,7 +5,13 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
@@ -24,7 +30,19 @@ public class JavaESClient {
 
     @Bean
     ElasticsearchClient getClient() {
-        RestClient restClient = RestClient.builder(new HttpHost(esHost, 443, "https")).build();
+
+        final CredentialsProvider cp = new BasicCredentialsProvider();
+        cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(esUser, esPass));
+
+        RestClient restClient = RestClient.builder(new HttpHost(esHost, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder.setDefaultCredentialsProvider(cp);
+                    }
+                })
+                .build();
+
         ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         return new ElasticsearchClient(transport);
     }
--- src/main/java/com/rmacd/queue/GeoJsonResponseHandler.java
+++ src/main/java/com/rmacd/queue/GeoJsonResponseHandler.java
@@ -1,7 +1,8 @@
 package com.rmacd.queue;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch.core.IndexRequest;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import com.google.gson.Gson;
 import com.rmacd.models.mdb.FeatureCollectionWrapper;
 import com.rmacd.repos.mdb.FeatureCollectionRepo;
 import org.apache.http.HttpResponse;
@@ -13,14 +14,23 @@ import org.geotools.feature.FeatureIterator;
 import org.geotools.feature.simple.SimpleFeatureImpl;
 import org.geotools.geojson.feature.FeatureJSON;
 import org.geotools.geojson.geom.GeometryJSON;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.referencing.CRS;
 import org.locationtech.jts.geom.Geometry;
 import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.UUID;
+
+import static org.geotools.referencing.crs.DefaultGeographicCRS.WGS84;
 
 public class GeoJsonResponseHandler implements ResponseHandler<GeometryJSON> {
 
@@ -28,6 +38,18 @@ public class GeoJsonResponseHandler implements ResponseHandler<GeometryJSON> {
 
     final FeatureCollectionRepo featureCollectionRepo;
     final ElasticsearchClient esClient;
+
+    // MathTransform to transform from source CRS to target CRS
+    static MathTransform transform;
+
+    static {
+        try {
+            CoordinateReferenceSystem sourceCRSObj = CRS.decode("EPSG:27700");
+            transform = CRS.findMathTransform(sourceCRSObj, WGS84);
+        } catch (FactoryException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public GeoJsonResponseHandler(
             FeatureCollectionRepo featureCollectionRepo, ElasticsearchClient esClient
@@ -50,14 +72,21 @@ public class GeoJsonResponseHandler implements ResponseHandler<GeometryJSON> {
                 feature.getIdentifier().getID();
                 // add geo to list of geos
                 Geometry geometry = (Geometry) ((SimpleFeatureImpl)feature).getDefaultGeometry();
+                Geometry reprojected = JTS.transform(geometry, transform);
 
-//                esClient.index(i -> i
-//                        .index("planning-features")
-//                        .id(UUID.randomUUID().toString())
-//                        .document(geometry)
-//                );
+                Gson gson = new Gson();
+                Object esObj = gson.fromJson(new GeometryJSON(6).toString(reprojected), Object.class);
 
+                IndexResponse r = esClient.index(i -> i
+                        .index("planning-features")
+                        .id(UUID.randomUUID().toString())
+                        .withJson(new StringReader(new GeometryJSON(6).toString(reprojected)))
+                );
+
+                logger.info("Wrote document {}", r.toString());
             }
+        } catch (TransformException e) {
+            throw new RuntimeException(e);
         } finally {
             iterator.close();
         }
